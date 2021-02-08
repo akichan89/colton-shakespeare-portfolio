158bf4ea1a82cfcd6de940b3d63889b6ad2f5318
Autumn Gehring <autumngehring@Autumns-MBP.hsd1.ut.comcast.net>
2021-02-08 15:06:25 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header';
+import About from './components/About';
+//import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+//import Testimonials from './components/Testimonials';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+//import ContactForm from './components/ContactForm';
+import ContactForm from './ContactMe';
+
+import "./App.css";
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        
+        <Portfolio resumeData={resumeData}/>
+        
+        <Router>
+          <Switch>
+            <Route path="/Contact" component={ContactForm} />
+          </Switch>
+        </Router>
+        <ContactForm />
+        <Footer resumeData={resumeData}/>
+        
+      </div>
+      
+);
+}
+}
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ContactForm from './ContactForm.js';
+
+export default class Header extends Component {
+    render() {
+        let resumeData = this.props.resumeData;
+            return (
+                <React.Fragment>
+                    <header id="home">
+                        <nav id="nav-wrap">
+                            <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show Navigation</a>
+                            <ul id="nav" className="nav">
+                                <li className="current">
+                                    <a className="smoothscroll" href="#home">Home</a>
+                                </li>
+                                <li>
+                                    <a href="https://drive.google.com/file/d/1__cmyBf5_pr57aMpGyKzEgqruH9tEBhl/view?usp=sharing" target="_blank">Resume</a>
+                                </li>
+                                <li>
+                                    <a className="smoothscroll" href="#about">About Me</a>
+                                </li>
+                                <li>
+                                <a className="smoothscroll" href="/ContactForm">Contact</a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/in/colton-shakespeare-8b1301133/" target="_blank">LinkedIn</a>
+                                </li>
+                                
+                            </ul>
+                        </nav>
+                        
+                        <div className="row banner">
+                            <div className="banner-text">
+                                <div className="responsive-headline">I am</div>
+                                <h1 className="headerName">Colton Shakespeare</h1>
+                                <div className="headerHeadline">{resumeData.role}{resumeData.roleDescription}</div>
+                                <hr/>
+                                <ul className="social">
+                                    {
+                                        resumeData.socialLinks&&resumeData.socialLinks.map(item => {
+                                            return(
+                                                <li key={item.name}>
+                                                    <a href={item.url} target="">
+                                                        <i className={item.className}></i>
+                                                    </a>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                        </div>
+                        <p className="scrolldown">
+                            <a className="smoothscroll" href="#about">
+                                <i className="icon-down-circle"></i>
+                            </a>
+                        </p>
+                    </header>
+                </React.Fragment>
+
+);
+}
+}
